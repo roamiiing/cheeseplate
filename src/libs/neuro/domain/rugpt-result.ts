1224997d1ba33da4e61b3672dfe2b9ef29eb2e8d
Vitaliy Lyovkin <unf.hwm@ya.ru>
2022-07-26 01:15:46 +0700

    fix(neuro): count ticks as valid end chars for ru-gpt

--- src/libs/neuro/domain/rugpt-result.ts
+++ src/libs/neuro/domain/rugpt-result.ts
@@ -7,7 +7,7 @@ export const processRuGptResult = (result: string) => {
   const stripped = stripAfter(result, RUGPT_MAX_LENGTH, true)
 
   // also remove comma and such characters from the end
-  const removed = stripped.replace(/([^?!.)}\]])$/, '')
+  const removed = stripped.replace(/([^?!.)}\]"'`])$/, '')
 
   return removed
 }
