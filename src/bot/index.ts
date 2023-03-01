fdea881896579dfa40dc90856d99091b18e3a103
Vitaliy Lyovkin <unf.hwm@ya.ru>
2023-03-01 19:38:48 +0700

    chore: enable source maps in production

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,6 +1,9 @@
 if (process.env.NODE_ENV !== 'production') {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('dotenv').config()
+} else {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('source-map-support').install()
 }
 
 import { RootController } from './root.controller'
